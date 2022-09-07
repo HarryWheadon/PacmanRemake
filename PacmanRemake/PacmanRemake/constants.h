34675f93c22f05e4b079f9548135f444e7ec0324
HarryWheadon <91532361+HarryWheadon@users.noreply.github.com>
2022-09-07 15:53:23 +0100

    updated

--- PacmanRemake/PacmanRemake/constants.h
+++ PacmanRemake/PacmanRemake/constants.h
@@ -1,10 +1,10 @@
 #pragma once
 //Dimensions
-#define SCREEN_WIDTH 980
-#define SCREEN_HEIGHT 1085
-#define MOVEMENTSPEED 600.0f
-#define MAP_HEIGHT 30
-#define MAP_WIDTH 34
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 544
+#define MOVEMENTSPEED 300.0f
+#define MAP_HEIGHT 17
+#define MAP_WIDTH 15
 #define TILE_WIDTH 32
 #define TILE_HEIGHT 32
 #define ANIMATION_DELAY 0.20f
